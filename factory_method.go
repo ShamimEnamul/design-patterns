57905fe0b9da6e9e0b14bca49a0b3d7965f388fb
enamul.shamim <enamul.shamim@trucklagbe.com>
2023-01-23 11:18:56 +0600

    first change

---
+++ factory_method.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+/* Product interface */
+type IGun interface {
+	setName(name string)
+	setPower(power int)
+	getName() string
+	getPower() int
+}
+
+/* Concrete product */
+type Gun struct {
+	name  string
+	power int
+}
+
+func (g *Gun) setName(name string) {
+	g.name = name
+}
+
+func (g *Gun) getName() string {
+	return g.name
+}
+
+func (g *Gun) setPower(power int) {
+	g.power = power
+}
+
+func (g *Gun) getPower() int {
+	return g.power
+}
+
+/* Concrete product*/
+type Ak47 struct {
+	Gun
+}
+
+func newAk47() IGun {
+	return &Ak47{
+		Gun: Gun{
+			name:  "AK47 gun",
+			power: 4,
+		},
+	}
+}
+
+type Musket struct {
+	Gun
+}
+
+func newMusket() IGun {
+	return &Musket{
+		Gun: Gun{
+			name:  "Musket gun",
+			power: 1,
+		},
+	}
+}
+
+/* Factory method */
+func getGun(gunType string) (IGun, error) {
+	if gunType == "ak47" {
+		return newAk47(), nil
+	}
+	if gunType == "musket" {
+		return newMusket(), nil
+	}
+	return nil, fmt.Errorf("Wrong gun type passed")
+}
+
+/* Client code */
+func main() {
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
